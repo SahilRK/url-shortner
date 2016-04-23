e6d2b83872dae252bc7719e27e10587feb16bbf6
Sahil_Kulkarni <sahil.rordev@gmail.com>
2016-04-24 01:47:14 +0530

    added short-url functionality test cases

--- test/controllers/short_urls_controller_test.rb
+++ test/controllers/short_urls_controller_test.rb
@@ -1,12 +1,35 @@
 require 'test_helper'
 
 class ShortUrlsControllerTest < ActionController::TestCase
-  test "should get new" do
+  def valid_user
+    @valid_user = User.create(uname: "url-test-user", email: "usl-user@email.com", password: "url-user")
+    session[:user_id] = @valid_user.id
+  end
+
+  test "should be authenticated user to create short url" do
+    get :new
+    assert_redirected_to log_in_path
+  end
+
+  test "should be able to view url generation form" do
+    valid_user
     get :new
     assert_response :success
   end
 
-  test "should get index" do
+  test "should be able to create new url" do
+    assert_difference('ShortUrl.count') do
+      post :create, short_url: {original_url: "www.google.com/test123", shorty: "err12", visit_counts: " ", user_id: "#{valid_user}"}
+    end
+  end
+
+  test "should be authenticated user to view short urls" do
+    get :index
+    assert_redirected_to log_in_path
+  end
+
+  test "should be able to show short urls" do
+    valid_user
     get :index
     assert_response :success
   end
--- test/models/short_url_test.rb
+++ test/models/short_url_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ShortUrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @valid_user = User.new(uname: "url-test-user", email: "usl-user@email.com", password: "url-user")
+
+    @short_url = ShortUrl.new(original_url: "www.google.com/test123", shorty: "err12", visit_counts: " ", user_id: "#{@valid_user.id}")
+  end
+
+  test "uniqueness of original url" do
+    short_url_dup = @short_url.dup
+    @short_url.save
+    short_url_dup.save
+    assert_equal "has already been taken",short_url_dup.errors[:original_url].first
+  end
+
+  test "generate unique short url" do
+    @short_url_sec = ShortUrl.new(original_url: "www.google.com/test1234", visit_counts: " ", user_id: "#{@valid_user.id}")
+    @short_url.save
+    @short_url_sec.generate_short_url
+    @short_url_sec.save
+    assert_not_equal @short_url.shorty, @short_url_sec.shorty
+  end
 end
